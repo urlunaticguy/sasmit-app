81140155e960f52d95dd07dc18bd71e9a847f09c
Souvik Das <souvikvillain@icloud.com>
2024-01-12 02:20:45 +0530

    fixes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -102,8 +102,9 @@ export default async function handler(req, res) {
         if (ress.acknowledged === true) {
           res.status(200).json({userCreated: true});
         }
+      } else {
+        res.status(200).json({userCreated: false});
       }
-      res.status(200).json({userCreated: false});
     } else if (val === "login") {
       const ress = await loginUser(email, password);
       console.log(ress);
